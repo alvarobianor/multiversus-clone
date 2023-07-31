33d2111d4de21959295b25af9b247f5255d65853
Álvaro Medeiros <alvaro.medeiros@hp.com>
2023-07-31 18:51:44 -0300

    Adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-black': '#c7c7c7',
+        'primary-black': '#1A232E',
         'secondary-white': '#c7c7c7',
       },
       transitionTimingFunction: {
